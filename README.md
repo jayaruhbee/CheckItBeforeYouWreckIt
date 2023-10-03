3b5eaaaf88490d6c0bef2f4918663cc9dc81f9b0
jayaruhbee <ottoman-fruity.0m@icloud.com>
2023-10-03 17:03:30 -0400

    initial project

---
+++ README.md
@@ -0,0 +1,20 @@
+# Purchase Prequalifier Tool
+
+![Preview](src/assets/CheckIt.png)
+
+## Overview
+
+Purchase Prequalifier Tool, a simple web application designed to help you make informed and mindful purchasing decisions. It allows you to assess potential purchases against a set of thoughtful questions to ensure the purchase aligns with your financial goals.
+
+## Features
+
+- Interactive questionnaire for assessing purchase decisions.
+- Sequential presentation of questions and easy navigation.
+- Clear recommendations based on responses.
+
+## Getting Started
+
+### Prerequisites
+
+- A history of poor financial decision making.
+
---
+++ src/components/Home.jsx
@@ -0,0 +1,52 @@
+import { useNavigate } from "react-router-dom";
+import { useState } from "react";
+
+const Home = () => {
+  const [selectedCategory, setSelectedCategory] = useState(null);
+  const navigate = useNavigate();
+
+  const handleCategoryClick = (category) => {
+    setSelectedCategory(category);
+    navigate(`questions/${category}`);
+  };
+
+  const headerStyle = {
+    color: "white",
+    fontSize: "54px",
+  };
+
+  return (
+    <>
+      <main className="h-[100vh] w-[100vw]">
+        <h1 className="poppins text-white text-6xl flex justify-center mt-6">
+          Check It Before You Wreck It
+        </h1>
+
+        <div className="options-container flex justify-center mt-[250px]">
+          <div className="cards-container poppins flex flex-row gap-3 ">
+            <button
+              onClick={() => handleCategoryClick("Food")}
+              className="food-card card sintony bg-yellow-300 hover:scale-110 transition-transform duration-400 transform hover:bg-yellow-300 cursor-pointer text-black text-lg p-8"
+            >
+              FOOD
+            </button>
+
+            <button
+              onClick={() => handleCategoryClick("Retail")}
+              className="retail-card card sintony bg-yellow-300 hover:scale-110 transition-transform duration-400 transform hover:bg-yellow-300 cursor-pointer text-black text-lg p-8"
+            >
+              RETAIL
+            </button>
+            <button
+              onClick={() => handleCategoryClick("Service")}
+              className="service-card card sintony bg-yellow-300 hover:scale-110 transition-transform duration-400 transform hover:bg-yellow-300 cursor-pointer text-black text-lg p-8"
+            >
+              SERVICE
+            </button>
+          </div>
+        </div>
+      </main>
+    </>
+  );
+};
+export default Home;
